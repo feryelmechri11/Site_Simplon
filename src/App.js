c83f10c38664933fef0b41755ecccb27ca8a9a51
Feryel <feryelmechrisimplon@gmail.com>
2020-05-05 12:17:15 +0100

    add router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
-import React from 'react';
-import './App.css';
-import Contact_form  from  './contact/contact'
-import './contact/contact.css';
-import Footer from './footer/footer'
-import './footer/footer.css';
-import SocialFollow from "./SocialFollow/SocialFollow"
-import Map  from  './map/map'
-
+import React from "react";
+import "./App.css";
+import Contact_form from "./contact/contact";
+import "./contact/contact.css";
+import Footer from "./footer/footer";
+import "./footer/footer.css";
+import SocialFollow from "./SocialFollow/SocialFollow";
+import Map from "./map/map";
 
 function App() {
   return (
     <div className="App">
-
-     <Contact_form />
-     <Footer />
-    
-
-
-
-
-
-
-
-      </div>
+      <Router>
+        <Route path="/Contact_form" exact component={Contact_form} />
+      </Router>
+      <Contact_form />
+      <Footer />
+    </div>
   );
 }
 
